4c6486152692a90e26c223dc29436b5c49030eeb
EfazIbrahim <efazibrahim01@gmail.com>
2024-09-17 18:07:14 -0400

    navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/studentHome';
+import Gpa from './pages/studentGpa';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>efaz</h1>
-        
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <nav>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/gpa">GPA</Link>
+                    </li>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/gpa" element={<Gpa />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
