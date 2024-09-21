0a37a5ca18949e25629c4060c47bfe62afcfe24b
EfazIbrahim <efazibrahim01@gmail.com>
2024-09-20 21:52:50 -0400

    background stuff

--- src/navigation/studentNav.js
+++ src/navigation/studentNav.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from '../pages/studentPages/studentHome';
 import Gpa from '../pages/studentPages/studentGpa';
+import './student.css';
+import logoWhite from '../assets/logo.png';
+
 
 function StudentNav() {
     return (
-        <div>
+        <div className="student">
             <nav className="student-navbar">
-                <li>
-                    <Link to="home">Home</Link>
-                </li>
-                <li>
-                    <Link to="gpa">GPA</Link>
-                </li>
+                <img className="student-logo" src={logoWhite} alt="Degree Bulls Logo"/>
+                <Link to="home">Home</Link>
+                <Link to="gpa">GPA</Link>
             </nav>
             <h1>Student Navigation</h1>
             <Routes>
