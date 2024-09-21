d5a0f23c95a92c19e06c1ec3565aa0696078d71a
Kraxer <farhan.s.abrar@gmail.com>
2024-09-20 21:53:00 -0400

    Something done

--- src/pages/testingAuth.js
+++ src/pages/testingAuth.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../App.css';
+
+
 
 function Auth() {
     const navigate = useNavigate();
@@ -9,11 +12,11 @@ function Auth() {
     };
 
     return (
-        <div>
+        <div className="auth-container">
             <h2>Select Your Role</h2>
-            <button onClick={() => handleNavigation('student')}>Student</button>
-            <button onClick={() => handleNavigation('advisor')}>Advisor</button>
-            <button onClick={() => handleNavigation('staff')}>Staff</button>
+            <button className="auth-button" onClick={() => handleNavigation('student')}>Student</button>
+            <button className="auth-button" onClick={() => handleNavigation('advisor')}>Advisor</button>
+            <button className="auth-button" onClick={() => handleNavigation('staff')}>Staff</button>
         </div>
     );
 }
