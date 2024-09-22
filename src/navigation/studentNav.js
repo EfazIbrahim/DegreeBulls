7124c50454cf74ef056e7982e8a29bde70d7ecba
EfazIbrahim <efazibrahim01@gmail.com>
2024-09-22 17:06:07 -0400

    fixed nav

--- src/navigation/studentNav.js
+++ src/navigation/studentNav.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import Home from '../pages/studentPages/studentHome';
 import Gpa from '../pages/studentPages/studentGpa';
 import './student.css';
 import logoWhite from '../assets/logo.png';
@@ -12,13 +11,13 @@ function StudentNav() {
         <div className="student">
             <nav className="student-navbar">
                 <img className="student-logo" src={logoWhite} alt="Degree Bulls Logo"/>
-                <Link to="home">Home</Link>
+                <Link to="/student">Home</Link>
                 <Link to="gpa">GPA</Link>
             </nav>
-            <h1>Student Navigation</h1>
+            <h1>Student Home</h1>
             <Routes>
-                <Route path="home" element={<Home />} />
                 <Route path="gpa" element={<Gpa />} />
+                <Route path="/student" element={<StudentNav />} />
             </Routes>
         </div>
     );
