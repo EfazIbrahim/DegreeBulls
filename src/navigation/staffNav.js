2c29ea74da4c3d7b8c24e55611f4188d045d93b2
Kraxer <farhan.s.abrar@gmail.com>
2024-09-22 18:23:44 -0400

    Commiting new home nav from Efaz

---
+++ src/navigation/staffNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import Gpa from '../pages/studentPages/studentGpa';
+import './student.css';
+import logoWhite from '../assets/logo.png';
+
+function StudentNav() {
+    return (
+        <div className="student">
+            <nav className="student-navbar">
+                <img className="student-logo" src={logoWhite} alt="Degree Bulls Logo"/>
+                <Link to="/student">Home</Link>
+                <Link to="gpa">GPA</Link>
+            </nav>
+            <h1>Student Home</h1>
+            <Routes>
+                <Route path="gpa" element={<Gpa />} />
+                <Route path="/student" element={<StudentNav />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default StudentNav;
