327239d23d52f6b6dd025e58c70a55a08023767a
EfazIbrahim <efazibrahim01@gmail.com>
2024-11-16 19:04:04 -0500

    changed home to courses

--- src/navigation/studentNav.js
+++ src/navigation/studentNav.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Gpa from '../pages/studentPages/studentGpa';
-import StudentHome from '../pages/studentPages/studentHome';
+import StudentCourses from '../pages/studentPages/studentCourses';
 import './studentNav.css';
 import logoWhite from '../assets/logo.png';
 
@@ -12,13 +12,13 @@ function StudentNav() {
         <div className="student">
             <nav className="student-navbar">
                 <img className="student-logo" src={logoWhite} alt="Degree Bulls Logo"/>
-                <Link className="student-links" to="/student">Home</Link>
+                <Link className="student-links" to="/student">Course</Link>
                 <Link className="student-links" to="gpa">GPA</Link>
             </nav>
             <Routes>
                 <Route path="gpa" element={<Gpa />} />
                 <Route path="/student" element={<StudentNav />} />
-                <Route path="/" element={<StudentHome />} />
+                <Route path="/" element={<StudentCourses />} />
             </Routes>
         </div>
     );
