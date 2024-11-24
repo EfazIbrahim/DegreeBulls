25baad29fe9cb6c6a86f4ec3a4a24ae908296983
EfazIbrahim <efazibrahim01@gmail.com>
2024-11-24 03:34:08 -0500

    course list

--- src/pages/studentPages/studentCourses.js
+++ src/pages/studentPages/studentCourses.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './student.css';
-import { GetCourses} from './studentFunctions.js';
+import { GetCourses } from './studentFunctions.js';
 import { useAuth } from '../../context/AuthContext.js';
 
 function StudentCourses() {
     const { currentUser } = useAuth();
-    const viewcourse = async () => {
+    const [courses, setCourses] = useState([]);
+
+    const getCourse = async () => {
         try {
             const courses = await GetCourses(currentUser);
-            console.log(courses);
+            setCourses(courses);
         } catch (error) {
             console.error('Error fetching courses:', error);
         }
     };
+
+    useEffect(() => {
+        getCourse();
+    }, []);
+
     return (
         <div className="student">
-            <h1 className="student-header">Student Courses</h1>
-            <button className="student-button" onClick={viewcourse}>Log Course</button>
+            <h1 className="student-header">Courses</h1>
+            <div className="student-semester">
+                {courses.map((course, index) => (
+                    <div key={index}>
+                        <h2 className="student-label">{course.semester}:</h2>
+                        <div>
+                            {course.courses.map((c, idx) => (
+                                <h1 className="student-courses" key={idx}>{c.course}: {c.grade}</h1>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
