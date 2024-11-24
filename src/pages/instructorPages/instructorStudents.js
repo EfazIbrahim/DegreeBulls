d0c9ab5e5740885de9b0179db491cd80cc96bd54
ishraqsadik <ishraqsadik2003@gmail.com>
2024-11-24 04:21:08 -0500

    instructor incomplete

--- src/pages/instructorPages/instructorStudents.js
+++ src/pages/instructorPages/instructorStudents.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './instructor.css';
+import { GetInstructorStudents, AssignGrade } from './instructorFunctions.js';
+import { useAuth } from '../../context/AuthContext.js';
+
 function InstructorStudents() {
+    const { currentUser } = useAuth();
+    const viewstudents = async () => {
+        try {
+            const instructor = await GetInstructorStudents(currentUser);
+            console.log(instructor);
+        } catch (error) {
+            console.error('Error fetching instructor:', error);
+        }
+    }
+    const assigngrade = async () => {
+        try {
+            const courses = await AssignGrade(currentUser);
+            console.log(courses);
+        } catch (error) {
+            console.error('Error fetching courses:', error);
+        }
+    }
     return (
-        <div className="instrcutor">
+        <div className="instructor">
             <h1 className="instructor-header">Students</h1>
+            <button className="instructor-button" onClick={viewstudents}>Students</button>
+            <button className="instructor-button" onClick={assigngrade}>Assign Grade</button>
         </div>
     );
 }
