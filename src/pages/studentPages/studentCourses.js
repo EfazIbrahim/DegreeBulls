7369fa023280adca0bc54d6e3996d32f00cc4331
EfazIbrahim <efazibrahim01@gmail.com>
2024-11-23 23:08:19 -0500

    debug button

--- src/pages/studentPages/studentCourses.js
+++ src/pages/studentPages/studentCourses.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './student.css';
 function StudentCourses() {
+    const handleButtonClick = async () => {
+        try {
+            const courses = await getCourses();
+            console.log(courses);
+        } catch (error) {
+            console.error('Error fetching courses:', error);
+        }
+    };
     return (
         <div className="student">
             <h1 className="student-header">Student Courses</h1>
+            <button className="student-button" onClick={handleButtonClick}>Log Course</button>
         </div>
     );
 }
