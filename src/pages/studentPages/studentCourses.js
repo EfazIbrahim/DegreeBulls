7c0d7ef20e3a57fbfc1556266e70d4045bdb1390
EfazIbrahim <efazibrahim01@gmail.com>
2024-11-24 02:53:53 -0500

    ef

--- src/pages/studentPages/studentCourses.js
+++ src/pages/studentPages/studentCourses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './student.css';
-import { GetCourses, calculateGPA } from './studentFunctions.js';
+import { GetCourses} from './studentFunctions.js';
 import { useAuth } from '../../context/AuthContext.js';
 
 function StudentCourses() {
@@ -13,19 +13,10 @@ function StudentCourses() {
             console.error('Error fetching courses:', error);
         }
     };
-    const viewgpa = async () => {
-        try {
-            const gpa = await calculateGPA(currentUser);
-            console.log(gpa)
-        } catch (error) {
-            console.error('Error fetching gpa:', error);
-        }
-    };
     return (
         <div className="student">
             <h1 className="student-header">Student Courses</h1>
             <button className="student-button" onClick={viewcourse}>Log Course</button>
-            <button className="student-button" onClick={viewgpa}>Log GPA</button>
         </div>
     );
 }
--- src/pages/studentPages/studentGpa.js
+++ src/pages/studentPages/studentGpa.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './student.css';
+import { calculateGPA } from './studentFunctions.js';
+import {useAuth} from "../../context/AuthContext";
+
 function Gpa() {
+    const { currentUser } = useAuth();
+    const viewgpa = async () => {
+        try {
+            const gpa = await calculateGPA(currentUser);
+            console.log(gpa)
+        } catch (error) {
+            console.error('Error fetching gpa:', error);
+        }
+    };
     return (
         <div className="student">
             <h1 className="student-header">Student GPA</h1>
+            <button className="student-button" onClick={viewgpa}>Log GPA</button>
         </div>
     );
 }
