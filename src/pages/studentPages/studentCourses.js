061cae4ef888881973eeb2b884ac83803012cb9a
EfazIbrahim <efazibrahim01@gmail.com>
2024-11-24 01:05:18 -0500

    ui

--- src/pages/studentPages/studentCourses.js
+++ src/pages/studentPages/studentCourses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './student.css';
+// import { getCourses } from './studentFunctions';
 function StudentCourses() {
     const handleButtonClick = async () => {
         try {
@@ -11,8 +12,12 @@ function StudentCourses() {
     };
     return (
         <div className="student">
+            {/*<button className="student-button" onClick={handleButtonClick}>Log Course</button>*/}
             <h1 className="student-header">Student Courses</h1>
-            <button className="student-button" onClick={handleButtonClick}>Log Course</button>
+            <div className="student-semester">
+                <h2 className="student-label">Fall 2024</h2>
+            </div>
+
         </div>
     );
 }
